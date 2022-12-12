be0c51bf97e514ed6eeb86d81228096792bac006
Hugo Vanderhaegen <hugo.vanderhaegen@student.junia.com>
2022-12-12 10:05:07 +0100

    added Proto Bonus

---
+++ Proto_Bonus_Hugo/Assets/Scripts/Offensive.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public abstract class Offensive : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+
+    public abstract void Shoot();
+}
