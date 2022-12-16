7c51c9aa134201a70271b90da701db795a7ef97d
Alexis SEREUSE <alexis.sereuse@student.junia.com>
2022-12-16 11:26:10 +0100

    Camera position changed with SmoothDamp function and max/min distances to target implemented

--- Proto_camera_perso/Assets/Scripts/CameraController.cs
+++ Proto_camera_perso/Assets/Scripts/CameraController.cs
@@ -10,10 +10,13 @@ public class CameraController : MonoBehaviour
 
     [SerializeField] private float _cameraElevation;
     [SerializeField] private float _camNodeDistanceTolerance;
+    [SerializeField] private float _camPlayerDistance;
+    [SerializeField] private float _smoothTime;
     private float _cameraSpeed;
     private Vector3 _target;
     private Vector3 _targetCameraPosition;
     private int _indexNextNode;
+    private float _maxTargetDist;
 
     void Start()
     {
@@ -24,18 +27,18 @@ public class CameraController : MonoBehaviour
 
         transform.position = new Vector3(nodes[0].transform.position.x, _cameraElevation, nodes[0].transform.position.z);
         _indexNextNode = 1;
+        _maxTargetDist = _camPlayerDistance + 10f;
     }
 
     void Update()
     {
-
-        transform.LookAt(players[0].transform);
-
         Vector3 sum = new Vector3(0, 0, 0);
+        //float speedSum = 0;
         float maxSpeed = 0f;
         foreach (GameObject player in players)
         {
             float speed = player.GetComponent<Rigidbody>().velocity.magnitude;
+            /*speedSum += speed;*/
             if (speed > maxSpeed)
             {
                 maxSpeed = speed;
@@ -43,17 +46,24 @@ public class CameraController : MonoBehaviour
 
             sum += player.transform.position;
         }
+        /*_cameraSpeed = speedSum / players.Length;*/
         _cameraSpeed = maxSpeed;
         _target = sum / players.Length;
 
         this.transform.LookAt(_target);
 
         _targetCameraPosition = nodes[_indexNextNode].transform.position + new Vector3(0, _cameraElevation, 0);
-        Vector2 distToNextPos = new Vector2(transform.position.x, transform.position.z) - new Vector2(nodes[_indexNextNode].transform.position.x, nodes[_indexNextNode].transform.position.z);
+        Vector3 distToNextPos = new Vector3(nodes[_indexNextNode].transform.position.x, 0, nodes[_indexNextNode].transform.position.z) - new Vector3(transform.position.x, 0, transform.position.z);
+        Vector3 distToTarget = _target - this.transform.position;
+        Vector3 speedVector = _cameraSpeed * distToNextPos.normalized;
 
-        if (distToNextPos.magnitude > _camNodeDistanceTolerance) 
+        if (distToNextPos.magnitude > _camNodeDistanceTolerance && distToTarget.magnitude > _camPlayerDistance && distToTarget.magnitude <= _maxTargetDist)
         {
-            this.transform.position = Vector3.Lerp(this.transform.position, _targetCameraPosition, _cameraSpeed * Time.deltaTime*0.1f);
+            this.transform.position = Vector3.SmoothDamp(this.transform.position, _targetCameraPosition, ref speedVector, _smoothTime);
+        }
+        else if (distToTarget.magnitude <= _camPlayerDistance || distToTarget.magnitude > _maxTargetDist) 
+        {
+            return;
         }
         else
         {
